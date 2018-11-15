19afdb27f5577ca8b67bfc255664de5979fad23a
Eugene Rubashko <e.rubashko@gmail.com>
2018-11-16 02:11:31 +0300

    Update deps

--- mix.exs
+++ mix.exs
@@ -32,14 +32,17 @@ defmodule Omitestee.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.0"},
-      {:phoenix_pubsub, "~> 1.1"},
-      {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix, "~> 1.3.4"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_html, "~> 2.10"},
+      {:phoenix_live_reload, "~> 1.0.0", only: :dev},
+      {:cowboy, "~> 1.0"},
+      {:plug_cowboy, "~> 1.0"},
+      {:ecto, "~> 3.0"},
+      {:scrivener_html, "~> 1.7"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
-      {:ecto, "~> 3.0"},
+      {:httpoison, "~> 1.4"},
       {:ok, "~> 2.0"}
     ]
   end
